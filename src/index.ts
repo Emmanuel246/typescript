c854e1e4730e2ce34bfa0cf2048a1ddaed5d9019
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-10-10 14:28:41 +0100

    Types Annotation

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,31 @@ let ages: number[] = [25, 27, 34]
 names.push('neoa')
 ages.push(84)
 
+
+// ..........................
+// type inference with arrays 
+let fruits = ["apples", "pears", "orange"]
+fruits.push('peaches')
+
+const f = fruits[3]
+
+
+let things = [, true, "String"]
+
+const t = things[0]
+// ................
 // ......................
 // object literals
 // ................
+let user: {firstName: string, age: number, id: number} = {
+    firstName: "Mario",
+    age: 30,
+    id: 1
+}
+
+user.firstName = "Peach"
+user.id = 2
+
 
 // ..........................
 // type inference with object literals
