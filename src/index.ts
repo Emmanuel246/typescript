b71cfc4ca889b04607e62a6819577b0b6852d780
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-10-10 14:13:06 +0100

    arrays

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello Ninjas!!!!!!!!!")
+// ---------------
+// arrays///-----------
+
+let names: string[] = ['Mario', "Luigi", "Peaach"]
+let ages: number[] = [25, 27, 34]
+
+names.push('neoa')
+ages.push(84)
+
+// ......................
+// object literals
+// ................
+
+// ..........................
+// type inference with object literals
+// ................
